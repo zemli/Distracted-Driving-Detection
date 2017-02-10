44ed27575dc9bbd8cf41e1b8b387a0d6c12db15b
zemli <zemingli777@gmail.com>
2017-02-10 02:01:58 -0500

    add optical flow

--- main.h
+++ main.h
@@ -21,10 +21,12 @@ cv::VideoCapture cap(0);
 int foundface;
 float landmarks[2 * stasm_NLANDMARKS];
 float landmarks0[2 * stasm_NLANDMARKS];
+cv::Mat_<unsigned char> img, frame;
 
 //Optical flow
-cv::Mat prev_gray;
+cv::Mat_<unsigned char> prev_frame;
 std::vector<cv::Point2f> features_prev, features_next;
+//std::vector<float> features_next, features_prev;
 std::vector<uchar> status;
 std::vector<float> err;
 
@@ -48,6 +50,7 @@ cv::Mat dst, detected_edges;
 /*
 function declared
 */
+int init();
 void compute();
 void featuresMotion();
 void wrinkles();
@@ -55,9 +58,48 @@ void cropImg(cv::Mat_<unsigned char> frame);
 void cannyEdge(cv::Mat_<unsigned char> frame);
 void gaborFilter(cv::Mat_<unsigned char> cropped);
 
+void force_points_into_image(std::vector<cv::Point2f> &pointV, int ncols, int nrows);
+
 /*
 function defined in main.cpp
 */
+
+int init(){
+	if(!cap.isOpened()){
+		std::cout<<"camera is not opened";
+		return 0;
+	}
+
+	cap >> img; // get a new frame from camera
+	cv::cvtColor(img, frame, CV_BGR2GRAY);
+
+	if(!frame.data){
+		std::cout<<"========== No image loaded !!!!!!! ===============";
+		return 0;
+	}
+
+	cap >> img; // get a new frame from camera
+	cv::cvtColor(img, frame, CV_BGR2GRAY);
+
+	if(!stasm_search_single(&foundface, landmarks, (const char*)frame.data, frame.cols, frame.rows, path, "../data")){
+		std::cout<<"stasm_search_single failed";
+		return 0;
+	}
+
+	if (!foundface)
+		std::cout<<"=============== No face found !!!!!!! ==============="<<std::endl;
+
+	stasm_force_points_into_image(landmarks,frame.cols, frame.rows);
+	//std::vector<cv::Point> features_prev(std::begin(landmarks), std::end(landmarks));
+	for(int i = 0; i < stasm_NLANDMARKS * 2; i += 2){
+		features_prev.push_back(cv::Point2f(landmarks[i], landmarks[i+1]));
+		
+	}
+
+	//features_prev.assign(landmarks, landmarks + stasm_NLANDMARKS * 2);
+
+	return 1;
+}
 
 void compute() {
 	lEyeLen = int(landmarks[30*2] - landmarks[34*2]);
@@ -186,4 +228,12 @@ void gaborFilter(cv::Mat_<unsigned char> frame){
 	dest.convertTo(viz,CV_8U,1.0/255.0);     // move to proper[0..255] range to show it
 
 	imshow("d",viz);
+}
+
+void force_points_into_image(std::vector<cv::Point2f> &pointV, int ncols, int nrows){
+	for (int i = 0; i < stasm_NLANDMARKS; i++)
+    {
+		pointV[i].x = MIN(MAX(pointV[i].x, 0.f), float(ncols-1));//MIN(MAX(x, min), max), force x to a value between min and max;
+		pointV[i].y = MIN(MAX(pointV[i].y, 0.f), float(nrows-1));
+    }
 }
